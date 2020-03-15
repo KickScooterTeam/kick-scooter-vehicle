fa504fb0d69941d10605a88b5c6551f008eafa04
Alexander <alex.lisitsa7@gmail.com>
2020-03-16 00:20:18 +0200

    Config properties files

---
+++ src/main/java/com/softserve/kickscooter/kickscootervehicle/KickScooterVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.kickscooter.kickscootervehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickScooterVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickScooterVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softserve/kickscooter/kickscootervehicle/KickScooterVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.kickscooter.kickscootervehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickScooterVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
